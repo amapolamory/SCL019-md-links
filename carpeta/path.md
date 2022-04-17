f5dc28747e3548a122cf775ca411f5fda7d2911a
amapolamory <amapola.rebeca.mory@gmail.com>
2022-04-17 14:48:10 -0400

    400 mesage

--- carpeta/path.md
+++ carpeta/path.md
@@ -1,3 +1,5 @@
 * [Arreglos](https://curriculum.laboratoria.la/es/topics/javascript/04-arrays)
  
  [spotify](http://www.spotiiiiiify.com/error/es)
+
+ (https://jestjs.io/docs/es-ES/getting-started)
--- main.js
+++ main.js
@@ -20,6 +20,7 @@ const extensionValid = (router) => {
     const ext = path.extname(router.toLowerCase());
     if (ext === ".md") {
         return true;
+        
     } else {
         console.log("No es un archivo md");
         return false;
@@ -54,7 +55,7 @@ const getLinks = (file, userPath) => {
                     line: i + 1,
                 };
                 arrayLinks.push(data);
-                //  validateLinks(data.href) //Ejecuta funcion anidada
+                 validateLinks(data.href) //Ejecuta funcion anidada
 
             }
             console.log(arrayLinks)
@@ -64,75 +65,82 @@ const getLinks = (file, userPath) => {
 
 }
 
-// function validateLinks(link) {
-//     return new Promise((resolve) => {
-//         const options = {
-//             method: 'HEAD',//El método HEAD pide una respuesta idéntica a la de una petición GET,(solicita una representación de un recurso específico.) pero sin el cuerpo de la respuesta.
-//             host: url.parse(link).host,
-//             port: 80,
-//             path: url.parse(link).pathname,
-//         };
-//         const req = http.request(options, (res) => {
-//             const nuevaData = {
-//                 linkname: link,
-//                 Code: res.statusCode,
-//                 status: res.statusCode <= 399,
-//             };
-//             console.log(`statusCode: ${res.statusCode}`)
-//             resolve(nuevaData);
-//         })
-
-//         req.on('error', (error) => {
-//             console.error(error);
-//             const newData = {
-//                 linkname: link,
-//                 status: false,
-//             };
-//             resolve(newData);
-//         });
-//         req.end()
-//     })
-    
-// }
-
-const validateLinks = (links) => {
-    const validate = links.map((link) =>
-      fetch(link.href).then((response) => {
-        return {
-          text: link.text,
-          href: link.href,
-          file: link.file,
-          line: link.line,
-          status: response.status,
-          statusText: response.statusText,
-          linkname: link,
-         Code: res.statusCode,   
-         status: res.statusCode <= 399,
+function validateLinks(link) {
+    return new Promise((resolve) => {
+        const options = {
+            method: 'HEAD',//El método HEAD pide una respuesta idéntica a la de una petición GET,(solicita una representación de un recurso específico.) pero sin el cuerpo de la respuesta.
+            host: url.parse(link).host,
+            port: 80,
+            path: url.parse(link).pathname,
         };
-        
-      })
+        const req = http.request(options, (res) => {
+            const nuevaData = {
+                linkname: link,
+                Code: res.statusCode,
+                status: res.statusCode <= 399,
+            };
+
+            if(nuevaData.status=res.statusCode ){
+            console.log(`statusCode: 400  failed`)
+            resolve(nuevaData);
+            }
+            else{
+                console.log(`statusCode: 200 ok`)
+                resolve(nuevaData);
+            }
+        })
+
+        req.on('error', (error) => {
+            console.error(error);
+            const newData = {
+                linkname: link,
+                status: false,
+            };
+            resolve(newData);
+        });
+        req.end()
+    })
     
-    );
-    return Promise.all(validate);
+}
+
+// const validateLinks = (links) => {
+//     const validate = links.map((link) =>
+//       fetch(link.href).then((response) => {
+//         return {
+//           text: link.text,
+//           href: link.href,
+//           file: link.file,
+//           line: link.line,
+//           status: response.status,
+//           statusText: response.statusText,
+//           linkname: link,
+//          Code: res.statusCode,   
+//          status: res.statusCode <= 399,
+//         };
+        
+//       })
+    
+//     );
+//     return Promise.all(validate);
 
 
  
-  };
+//   };
 
- const mdLinks = (files, option) => {
-    return new Promise((resolve, reject) => {
-      const links = fileValid(files);
-      if (option.validate) {
-        resolve(validateLinks(links));
+//  const mdLinks = (files, option) => {
+//     return new Promise((resolve, reject) => {
+//       const links = fileValid(files);
+//       if (option.validate) {
+//         resolve(validateLinks(links));
       
  
-      } else {
-        resolve(links);
-        console.log(`statusCode: ${response.statusCode}`)
-      }
-      reject(new TypeError('error'));
-    });
-  };
+//       } else {
+//         resolve(links);
+//         console.log(`statusCode: ${response.statusCode}`)
+//       }
+//       reject(new TypeError('error'));
+//     });
+//   };
 
 
 
@@ -147,7 +155,7 @@ exports.getLinks = getLinks;
 exports.existence = existence;
 exports.isFile = isFile;
 exports.validateLinks = validateLinks;
-exports.mdLinks = mdLinks;
+// exports.mdLinks = mdLinks;
 
 
 

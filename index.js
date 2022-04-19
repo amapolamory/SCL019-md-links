5f5a252610c40ae127fd5055c75680cbc6631ac4
amapolamory <amapola.rebeca.mory@gmail.com>
2022-04-19 11:03:41 -0400

    --validate --stats funcional

--- index.js
+++ index.js
@@ -2,86 +2,121 @@ const main = require("./main");
 let links = []
 
 
- const mdLinks = (path,options) => {
+const mdLinks = (path, options) => {
 
   let totalLinks = 0;
   let linksOk = 0;
-  let linkBroken =0;
+  let linkBroken = 0;
   let arraylink = [];
+  let duplicados= {};
 
   return new Promise((resolve) => {
-    
-    if(main.existence(path)){
+
+    if (main.existence(path)) {
 
       path = main.relToAbs(path);
 
-      if(main.isFile(path)){
+      if (main.isFile(path)) {
 
-        if(main.extensionValid(path)){
+        if (main.extensionValid(path)) {
 
-         const data = main.readFile(path);
-         main.getLinks(data,path)
-         .then((arrayLinks) => {
-          totalLinks = arrayLinks.length
-          
-          arrayLinks.forEach(element => {
-            links.push(element.href)
-          });
-    
+          const data = main.readFile(path);
+          main.getLinks(data, path)
+            .then((arrayLinks) => {
+              totalLinks = arrayLinks.length
 
-    
-            const promiseArr = links.map((url) => main.validateLinks(url).then((status) => {
-              arraylink.push(status);
-            //   console.table(arraylink);
-           
+              arrayLinks.forEach(element => {
+                links.push(element.href)
+              });
 
-              if(options.validate && !options.stats ){
-            //   console.log('Link : ', status.linkname, )
-            //   console.log('Status Code : ', status.Code)
-              if(status.status){
-            status.status='ok';
-              }else{
-              status.status='fail;'
-                  } 
+
+
+              const promiseArr = links.map((url) => main.validateLinks(url).then((status) => {
+                arraylink.push(status);
+
+                if (options.validate && !options.stats) {
+      
+                  if (status.status) {
+                    status.status = 'ok';
+                  } else {
+                    status.status = 'fail;'
+                  }
                 }
 
+
+              })
+                .catch((err) => {
+                  console.log('La ruta  no existe'.bgRed);
+                  console.log(err);
+                }));
+              return Promise.all(promiseArr);
+
+
+
+
+            }).then(() => {
+              if(options.validate && !options.stats){
+                console.table(arraylink);
+              }
+              if (!options.validate && options.stats) {
+
+                const busqueda = arraylink.reduce((acc, arraylink) => {
+                  acc[arraylink.linkname] = ++acc[arraylink.linkname] || 0;
+                  return acc;
+                }, {});
+                
+                 duplicados = arraylink.filter( (arraylink) => {
+                  return busqueda[arraylink.linkname];
+                });
+
+                console.log('total: ',arraylink.length)
+                console.log('unique: ' ,arraylink.length - duplicados.length )
+              }
+
+              
+              }).then(()=>{
+
+              if(options.validate && options.stats){
+
+                const busqueda = arraylink.reduce((acc, arraylink) => {
+                  acc[arraylink.linkname] = ++acc[arraylink.linkname] || 0;
+                  return acc;
+                }, {});
+                
+                 duplicados = arraylink.filter( (arraylink) => {
+                  return busqueda[arraylink.linkname];
+                });
+                
+              
+
+                arraylink.forEach(element => {
+                  if (element.status) {
+                    linksOk++
+                  } 
+                
+                  else  {
+                    linkBroken++
+
+                  }
+                });
+                console.log('Links unicos: ',arraylink.length-duplicados.length)
+                console.log('Links totales: ',arraylink.length)
+                console.log('Links Rotos : ', linkBroken)
+                
+              }
             })
-              .catch((err) => {
-                console.log('La ruta  no existe'.bgRed);
-                console.log(err);
-              }));
-            return Promise.all(promiseArr);
-           
 
-
-
-          }).then(() => {
-              console.table(arraylink)
-            if(!options.validate && options.stats ){
-              arraylink.forEach(element => {
-                   if(element.status){
-                     linksOk++
-                   }else{
-                     linkBroken++
-                   }
-               });
-
-               console.log('Links Correctos : ', linksOk)
-               console.log('Links Rotos : ', linkBroken)
-            }
-          })
-
-        }else{
+        } else {
           console.log('No es una archivo .md')
         }
-      }else{
+      } else {
         console.log('No es una archivo')
       }
 
-    }else{
+    } else {
       console.log('La ruta no existe')
     }
-   
+
 
   });
 };
--- path.md
+++ path.md
@@ -1,3 +1,6 @@
 * [Arreglos](https://curriculum.laboratoria.la/es/topics/javascript/04-arrays)
  
  [spotify](http://www.spotiiiiiify.com/error/es)
+  [spotify](http://www.spotiiiiiify.com/error/es)
+    [spotify](http://www.spotify.com/error/es)
+    [Promise - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Promise)
